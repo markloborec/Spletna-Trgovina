cbf275dc326448a305b8c2d5b5d96f5e78932d9e
matkomat <matevz.koren@scptuj.si>
2025-11-22 20:15:13 +0100

    Prvi frontend commit

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './homepage.html',
   styleUrls: ['./homepage.scss'],
 })
